d7d569a032124596adda5874d4194fb54e54e33a
SHINICHI <masaki.shinichi.1984@gmail.com>
2020-07-18 14:38:31 +0900

    crud app

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # 新規投稿画面へのルーティング
+  get "/users/new", to: "users#new"
+  get "/users", to: "users#index"
+  # 新規登録を行うためのルーティング
+  post "/users", to: "users#create"
+  delete "/users/:id", to: "users#destroy"
+  get "/users/:id/edit", to: "users#edit"
+  patch "/users/:id", to: "users#update"
+end
